ecdec4dccdbb6b49d79d9e9f920dc1f9544563cf
okawG <gsmt.7229@gmail.com>
2025-11-15 15:13:02 +0900

    feat:単発エフェクトに変更

--- Test/Assets/Scenes/EffectManager.cs
+++ Test/Assets/Scenes/EffectManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectManager : MonoBehaviour
@@ -6,7 +7,6 @@ public class EffectManager : MonoBehaviour
     public GameObject effectPrefab;
 
     Camera cam;
-    GameObject effect;
 
     void Start()
     {
@@ -22,7 +22,19 @@ public class EffectManager : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(1))
         {
-            effect.GetComponent<ParticleSystem>().Pause();
+            ParticleSystem[] allPS = FindObjectsOfType<ParticleSystem>();
+
+            foreach (var ps in allPS)
+            {
+                if (ps.isPaused)
+                {
+                    ps.Play();
+                }
+                else
+                {
+                    ps.Pause();
+                }
+            }
         }
     }
 
@@ -34,7 +46,7 @@ public class EffectManager : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            effect = Instantiate(effectPrefab, hit.point, Quaternion.identity);
+            Instantiate(effectPrefab, hit.point, Quaternion.identity);
         }
         else
         {
@@ -42,7 +54,7 @@ public class EffectManager : MonoBehaviour
             Vector3 pos = cam.ScreenToWorldPoint(
                 new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f)
             );
-            effect = Instantiate(effectPrefab, pos, Quaternion.identity);
+            Instantiate(effectPrefab, pos, Quaternion.identity);
         }
     }
 }
