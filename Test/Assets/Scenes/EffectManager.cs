ca3e488c276677324ab829e1fc38c81428a2e573
okawG <gsmt.7229@gmail.com>
2025-11-15 14:34:07 +0900

    feat:エフェクト再生シーン作成

---
+++ Test/Assets/Scenes/EffectManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    [Header("再生したいパーティクルプレハブ")]
+    public GameObject effectPrefab;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        // 左クリックでエフェクトを出す
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnEffectAtMouse();
+        }
+    }
+
+    void SpawnEffectAtMouse()
+    {
+        if (effectPrefab == null) return;
+
+        // クリック位置 → ワールド座標へ
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Instantiate(effectPrefab, hit.point, Quaternion.identity);
+        }
+        else
+        {
+            // 地面がない場合、Z=0 平面に出す例
+            Vector3 pos = cam.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f)
+            );
+            Instantiate(effectPrefab, pos, Quaternion.identity);
+        }
+    }
+}
